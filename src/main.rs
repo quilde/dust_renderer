788ece890853a416b403519402f38671b7db7720
quilde <enseli.nils@gmail.com>
2024-02-18 13:29:28 +0100

    back to old main

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use dust_renderer::{resize_window, setup, DustMain};
+use dust_renderer::{resize_window, setup, setup_single, DustMain};
 
 use tao::{
     dpi::{LogicalSize, PhysicalSize},
@@ -12,57 +12,39 @@ use wgpu::{Device, Surface, SurfaceConfiguration};
 use std::collections::HashMap;
 use std::rc::Rc;
 
-use glam::UVec2;
-
 fn main() {
     pollster::block_on(run());
 }
 
 pub async fn run() {
     let event_loop = EventLoop::new();
-    let window_beforemove = Some(
+    let mut window = Some(
         WindowBuilder::new()
             .with_decorations(true)
             .with_inner_size(tao::dpi::LogicalSize::new(300.0, 300.0))
             .with_min_inner_size(tao::dpi::LogicalSize::new(200.0, 200.0))
-            .with_transparent(true)
+            .with_transparent(false)
             .build(&event_loop)
             .unwrap(),
     );
-    let window_beforemove2 = Some(
-        WindowBuilder::new()
-            .with_decorations(true)
-            .with_inner_size(tao::dpi::LogicalSize::new(600.0, 600.0))
-            .with_min_inner_size(tao::dpi::LogicalSize::new(200.0, 200.0))
-            .with_transparent(true)
-            .build(&event_loop)
-            .unwrap(),
-    );
-    
-    let size1 = glam::UVec2{
-        x: window_beforemove.unwrap().inner_size().width,
-        y: window_beforemove.unwrap().inner_size().height,
-    };
-    
-    let size2 = glam::UVec2{
-        x: window_beforemove2.unwrap().inner_size().width,
-        y: window_beforemove2.unwrap().inner_size().height,
-    };
 
-    let (mut windows, mut surfaces, mut surface_configurations, devices, queues) =
-        setup(vec![&window_beforemove.unwrap(),&window_beforemove2.unwrap(),], 
-            vec![
-            size1,
-            size2,
-                ]).await;
+    let size = glam::UVec2 {
+        x: window.as_ref().unwrap().inner_size().width,
+        y: window.as_ref().unwrap().inner_size().height,
+    };
+    let (
+        device,
+        queue,
+        mut surface,
+        mut surface_configuration,
+        
+        
+    ) = setup_single(window.as_ref().unwrap(), size).await;
 
     //let mut dust = DustRenderer::new("dust label DustRenderer");
 
-    let mut dust_main1 = DustMain::new(&devices[0], &queues[0], &surface_configurations[0], size1);
-    dust_main1.setup(&devices[0], &queues[0]);
-    
-    let mut dust_main2 = DustMain::new(&devices[1], &queues[1], &surface_configurations[1], size2);
-    dust_main2.setup(&devices[1], &queues[1]);
+    let mut dust_main = DustMain::new(&device, &queue, &surface_configuration, size);
+    dust_main.setup(&device, &queue);
     //dust.add_plugin("dust_main plugin", Rc::new(dust_main) );
     //dust.prepare(&device, &queue, &surface_configuration);
     //env_logger::init();
@@ -80,7 +62,7 @@ pub async fn run() {
                 ..
             } => {
                 // drop the window to fire the `Destroyed` event
-                windows = None;
+                window = None;
             }
             Event::WindowEvent {
                 event: WindowEvent::Destroyed,
@@ -137,19 +119,19 @@ pub async fn run() {
             Event::MainEventsCleared => {
                 //if let w = window { //Some(w)}
                 //windows[&window_id].request_redraw();
-                if let Some(w) = &windows {
-                    windows.as_ref().unwrap().request_redraw();
+                if let Some(w) = &window {
+                    window.as_ref().unwrap().request_redraw();
                 }
             }
             Event::RedrawRequested(window_id) => {
                 println!("redrawing!\n");
                 dust_main.prepare(&device, &queue);
-                let size1 = &windows.as_ref().unwrap().inner_size();
+                let size1 = &window.as_ref().unwrap().inner_size();
                 let size = glam::UVec2 {
                     x: size1.width,
                     y: size1.height,
                 };
-                let result = render(
+                let result = dust_renderer::render(
                     &device,
                     &queue,
                     "label",
@@ -162,8 +144,8 @@ pub async fn run() {
                 match result {
                     Ok(_) => {}
                     // Reconfigure the surface if lost
-                    Err(wgpu::SurfaceError::Lost) => resize_window(
-                        physical_size,
+                    Err(wgpu::SurfaceError::Lost) => dust_renderer::resize_window(
+                        window.as_ref().unwrap().inner_size(),
                         &mut surface_configuration,
                         &device,
                         &mut surface,
@@ -188,3 +170,52 @@ pub async fn run() {
     });
 }
 
+pub fn render(
+    device: &wgpu::Device,
+    queue: &wgpu::Queue,
+    label: &str,
+    surface: &wgpu::Surface,
+    config: &wgpu::SurfaceConfiguration,
+    dust_main: &mut DustMain,
+    size: &glam::UVec2,
+) -> Result<(), wgpu::SurfaceError> {
+    let output = surface.get_current_texture()?;
+    let view = output
+        .texture
+        .create_view(&wgpu::TextureViewDescriptor::default());
+    let mut encoder = device.create_command_encoder(&wgpu::CommandEncoderDescriptor {
+        label: Some("Render Encoder"),
+    });
+
+    let mut encoder = dust_main.render_compute(encoder, &device, &queue, *size);
+
+    {
+        let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+            label: Some("Render Pass"),
+            color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                view: &view,
+                resolve_target: None,
+                ops: wgpu::Operations {
+                    load: wgpu::LoadOp::Clear(wgpu::Color {
+                        r: 0.1,
+                        g: 0.2,
+                        b: 0.3,
+                        a: 0.5,
+                    }),
+                    store: true,
+                    //store: wgpu::StoreOp::Store,
+                },
+            })],
+            depth_stencil_attachment: None,
+            //timestamp_writes: None,
+            //occlusion_query_set: None,
+        });
+
+        dust_main.render(&mut render_pass, &device);
+    }
+
+    queue.submit(std::iter::once(encoder.finish()));
+    output.present();
+
+    Ok(())
+}
